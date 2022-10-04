271b8cce4e18c19340fd8ebe365a8dfca574c07b
Fulin <944799613@qq.com>
2022-10-04 18:01:24 -0500

    Add project files.

---
+++ BankUnitTest/UnitTest1.cs
@@ -0,0 +1,63 @@
+namespace BankUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public BankAccount TestAccount { get; set; }
+        public decimal InitialBalance { get; set; }
+        public UnitTest1()
+        {
+            InitialBalance = 500; //初始余额 500元
+            TestAccount = new BankAccount(); 
+            TestAccount.balance = InitialBalance;
+        }
+
+        //Withdraw method should subtract the amount withdraw from the balance of the Account
+
+        [TestMethod]
+        public void AccountWithdraw_SubtractsFromBalance() //账户取款_从余额中减去  方法
+        { 
+
+            // Arranges 布置
+            decimal withdrawlAmount = 200.5m;  //取款 200.5
+            decimal assertedBalance = InitialBalance - withdrawlAmount; //过程后的余额
+
+            // Act 行为
+            TestAccount.Withdraw(withdrawlAmount); //测试 Program里面的方法，if语句
+
+            // Assert 决断，最后
+            Assert.AreEqual(assertedBalance, TestAccount.balance);  // Assert方法：将一个对象与另一个对象进行比较 （期待的，实际的）
+        }
+
+        [TestMethod]
+        public void AccountWithdraw_throwsExceptionWhenArgumentGreaterThanBalance() //账户取款_当参数大于平衡时抛出异常  方法
+        {
+            // Arrange
+            decimal currantBalance = 500;  //目前 500
+            BankAccount account = new BankAccount();  //还是与Program有关的 新设的 account 关键字
+            account.balance = currantBalance;  //让新的这个关键字 等于 500
+
+            decimal withdrawlAmount = 700; //取款 700
+
+            // act and assert
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                TestAccount.Withdraw(withdrawlAmount);     //Assert.ThrowsException 判断断言异常
+            });
+        }
+
+        [TestMethod]
+        public void AccountDeposit_AddToBalance() // 账户存款_添加到余额
+        {
+            // arrange
+            decimal depositAmount = 125.32m;   // 存款 125.32
+            decimal assertedBalance = depositAmount + InitialBalance;  // asserted = 存款 + 现在。
+
+            // act
+            TestAccount
+
+            //assert
+            Assert.AreEqual(assertedBalance, TestAccount.balance);
+        }
+    }
+}
---
+++ UnitTestingFirstDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Run");
+public class BankAccount 
+{
+    // balance 余额
+    // withdrawl 取款
+    private decimal _balance;
+    public decimal balance
+    {
+        get { return _balance; }
+        set { _balance = value; }
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if(balance >= amount)
+        {
+            balance -= amount;
+        }
+        else
+        {   // ArgumentException 参数异常
+            //ArgumentException是一个自定义“合法参数”的异常，ArgumentException类所接受的合法参数，是由程序员自行设置的。 FormatException当参数格式不符合调用的方法的参数规范时引发的异常. 虽然说是“方法的参数”，但是其实包含了绝大多数的格式异常，例如：索引的格式异常等等。
+            throw new ArgumentException("Withdraw exceeds available funds.");
+        }
+    }
+
+    // To be implemented 有待实施
+    public decimal Deposit(decimal amount)
+    {
+        if (amount >= 0)   // 如果存款 >= 0  就能存
+        {
+            balance += amount;
+
+            return balance;
+        }
+        else // 存款 <= 0 就告诉你是穷比
+        {
+            throw new ArgumentException("Amount need more than");
+        }
+
+    }
+
+}
